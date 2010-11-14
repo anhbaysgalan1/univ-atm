c76232c68199b59c6a59c6c9ae65a6753d5c6ac5
Helder Correia <me@heldercorreia.com>
2010-11-14 00:23:24 -0100

    Início do projecto.

---
+++ src/Application/Main.java
@@ -0,0 +1,17 @@
+
+package Application;
+
+/**
+ *
+ * @author heldercorreia
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
