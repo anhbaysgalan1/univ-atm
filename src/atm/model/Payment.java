156ea7b12cfe3cd8aa482fd33eeed85d407e7ee7
Miguel Ferreira <migfer25@hotmail.com>
2010-12-04 16:23:20 -0100

    Implementaçao de validaçao dos valores de referencia, entidade e montante no Payment

--- src/atm/model/Payment.java
+++ src/atm/model/Payment.java
@@ -23,6 +23,46 @@ public class Payment {
      * @param ammount    montante
      */
     public Payment(String entity, String reference, double ammount) {
+
+        ammount=Math.abs(ammount);
+        //Testa a referencia caso acho caracteres invalidos
+        try{
+            Integer.parseInt(reference);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("Referencia inválido");
+            }
+
+        //Verifica se a referencia introduzida é inferior a 9 digitos
+        if(reference.length()<9)
+                throw new IllegalArgumentException(
+                    "Referencia inválida - Caracteres em falta"
+                    );
+
+        //Verifica se a referencia introduzida é superior a 9 digitos
+        if(reference.length()>9)
+                throw new IllegalArgumentException(
+                    "Referencia inválida - Caracteres em excesso"
+                    );
+        //Testa a entidade caso acho caracteres invalidos
+        try{
+            Integer.parseInt(entity);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("Entidade inválido");
+            }
+
+        //Verifica se a entidade introduzida é inferior a 5 digitos
+        if(entity.length()<5)
+                throw new IllegalArgumentException(
+                    "Entidade inválida - Caracteres em falta"
+                    );
+
+        //Verifica se a entidade introduzida é superior a 5 digitos
+        if(entity.length()>5)
+                throw new IllegalArgumentException(
+                    "Entidade inválida - Caracteres em excesso"
+                    );
+
+
         this.entity    = entity;
         this.reference = reference;
         this.ammount   = ammount;
